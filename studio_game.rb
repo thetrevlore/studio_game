048ccd9da484a9ef52c58d46e719d6f879fefd7c
Trevor Storey <trevor@photoshelter.com>
2019-11-07 20:16:38 -0700

    [master] after chapter 3 numbers and strings

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,12 @@
-greeting = "Welcome!"
+name1 = 'larry'
+health1 = 60
 
-3.times do
-  puts greeting.upcase
-end
+name2 = 'curly'
+health2 = 60
 
-puts Time.now
+name3 = 'moe'
+health3 = 60
+
+puts "#{name1}'s health is #{health1 / 9}"
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
