92758d226ad980e04d37053d619a9103040a46aa
Trevor Storey <trevor@photoshelter.com>
2019-11-11 10:16:34 -0700

    [master] after chapter 9, arrays

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,22 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
+  def score
+    @name.length + @health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -23,11 +34,19 @@ player1 = Player.new('moe')
 player2 = Player.new('larry', 60)
 player3 = Player.new('curly', 125)
 
-# puts player1.inspect
-# puts player1.to_s
-# puts player2.to_s
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in the game:"
+
+# players.each do |player|
+#   puts player
+# end
+
+# players.pop
+# players.push(Player.new('shemp', 90))
+
+# players.each do |player|
+#   player.w00t
+#   player.blam
+#   puts player
+# end
