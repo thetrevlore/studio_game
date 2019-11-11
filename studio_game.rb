0851188757af3cf9906fcefa68a97a93eb755d11
Trevor Storey <trevor@photoshelter.com>
2019-11-11 10:31:48 -0700

    [master] after chapter 10, objects interacting

--- studio_game.rb
+++ studio_game.rb
@@ -30,23 +30,38 @@ class Player
   end
 end
 
+class Game
+  attr_reader :title
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      puts player
+    end
+  end
+
+end
+
+
 player1 = Player.new('moe')
 player2 = Player.new('larry', 60)
 player3 = Player.new('curly', 125)
 
-players = [player1, player2, player3]
-
-puts "There are #{players.size} players in the game:"
-
-# players.each do |player|
-#   puts player
-# end
-
-# players.pop
-# players.push(Player.new('shemp', 90))
-
-# players.each do |player|
-#   player.w00t
-#   player.blam
-#   puts player
-# end
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
