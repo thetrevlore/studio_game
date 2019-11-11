73cc03d1f0401a0070f06216ccd63e86d4f73001
Trevor Storey <trevor@photoshelter.com>
2019-11-11 08:52:52 -0700

    [master] after chapter 7 classes...previous commit was after chapter 6 methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,15 +1,33 @@
-def say_hello(name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+class Player
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
 end
 
-def time
-  current_time = Time.new
-  current_time.strftime('%I:%M:%S')
-end
+player1 = Player.new('moe')
+player2 = Player.new('larry', 60)
+player3 = Player.new('curly', 125)
 
-puts say_hello('larry', 60)
-puts say_hello('curly', 125)
-puts say_hello('moe')
-puts say_hello('shemp', 90)
-
-puts time
+# puts player1.inspect
+# puts player1.to_s
+# puts player2.to_s
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
