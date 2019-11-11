0c154b9f653be89e0ab59dc35d05f2100739ba00
Trevor Storey <trevor@photoshelter.com>
2019-11-11 10:40:22 -0700

    [master] after chapter 11, separate source files

---
+++ player.rb
@@ -0,0 +1,41 @@
+class Player
+  attr_accessor :name
+  attr_reader :health
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def score
+    @name.length + @health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+end
+
+if __FILE__ == $0
+  player = Player.new('moe')
+  puts player.name
+  puts player.health
+  player.w00t
+  puts player.health
+  player.blam
+  puts player.health
+end
