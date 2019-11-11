69e608b1176105f2fc1380d2f76edfb8be83d161
Trevor Storey <trevor@photoshelter.com>
2019-11-11 08:26:28 -0700

    [master] after chapter 7 classes

--- studio_game.rb
+++ studio_game.rb
@@ -1,30 +1,15 @@
-name1 = 'larry'
-health1 = 60
-puts "#{name1.capitalize}'s health is #{health1}"
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-name2 = 'curly'
-health2 = 125
-puts "#{name2.upcase} has a health of #{health2}"
+def time
+  current_time = Time.new
+  current_time.strftime('%I:%M:%S')
+end
 
-health2 = health1
-puts "#{name2.upcase} has a health of #{health2}"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
 
-health1 = 30
-puts "#{name1.capitalize}'s health is #{health1}"
-puts "#{name2.upcase} has a health of #{health2}"
-
-name3 = 'moe'
-health3 = 100
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-
-name4 = 'shemp'
-health4 = 90
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
-
-current_time = Time.new
-puts current_time.strftime("The game started on %A %m/%d/%Y at%l:%M%p")
-
-
-
-
-# puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+puts time
