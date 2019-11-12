3734024e2eaf91d67e921fe3dfefe3fb2b98cd87
Trevor Storey <trevor@photoshelter.com>
2019-11-11 19:58:13 -0700

    [master] after chapter 16, Symbols and Structs

--- game_turn.rb
+++ game_turn.rb
@@ -1,17 +1,12 @@
 require_relative 'die'
 require_relative 'player'
+require_relative 'treasure_trove'
 
 module GameTurn
   def self.take_turn(player)
     die = Die.new
     number_rolled = die.roll
-    # if number_rolled >= 5
-    #   player.w00t
-    # elsif number_rolled <= 2
-    #   player.blam
-    # else
-    #   puts "#{player.name} was skipped."
-    # end
+
     case number_rolled
       when 5..6
         player.w00t
@@ -20,5 +15,7 @@ module GameTurn
       when 1..2
         player.blam
     end
+    treasure = TreasureTrove.random
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
   end
 end
